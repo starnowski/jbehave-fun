b9cc5b59622b9fffbe5f72cf7444a3e94a7594f0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-06-21 00:55:28 +0200

    Added fun2 project

---
+++ fun2/src/main/java/com/github/starnowski/jbehave/fun/Cordinates.java
@@ -0,0 +1,38 @@
+package com.github.starnowski.jbehave.fun;
+
+public class Cordinates {
+
+    private int x;
+    private int y;
+
+    public Cordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cordinates that = (Cordinates) o;
+
+        if (x != that.x) return false;
+        return y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ fun2/src/main/java/com/github/starnowski/jbehave/fun/StringRenderer.java
@@ -0,0 +1,46 @@
+package com.github.starnowski.jbehave.fun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringRenderer {
+
+    private int width;
+    private int height;
+
+    private List<Cordinates> crosses = new ArrayList<Cordinates>();
+
+    public String asString() {
+        char[][] map = new char[height][width];
+        for (int raw = 0; raw < height; raw++) {
+            for (int col = 0; col < width; col++) {
+                map[raw][col] = '.';
+            }
+        }
+        for (Cordinates cor : crosses) {
+            map[cor.getY()][cor.getX()] = 'X';
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int raw = 0; raw < height; raw++) {
+            for (int col = 0; col < width; col++) {
+                sb.append(map[raw][col]);
+            }
+            if (raw < height - 1) {
+                sb.append(String.format("%n"));
+            }
+        }
+        return sb.toString();
+    }
+
+    public void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void toggleCellAt(int column, int row) {
+        Cordinates cor = new Cordinates(column, row);
+        if (!crosses.remove(cor)) {
+            crosses.add(cor);
+        }
+    }
+}
